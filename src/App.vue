9d27c78a7a02e232ffdd6323eaa14a4210cce8a5
Patrick A Lima <patrickalima98@live.com>
2019-03-05 17:48:07 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+ const { Gitlab } = require('gitlab');
+
+@Component({
+  name: 'App'
+})
+export default class App extends Vue {
+  mounted() {
+   const api = new Gitlab({
+    token: "ACCESS_TOKEN" // Can be created in your profile.
+  });
+  api.Projects.all({ maxPages: 1, perPage: 40 }).then((data: any) => {
+    console.log(data);
+  }); 
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
